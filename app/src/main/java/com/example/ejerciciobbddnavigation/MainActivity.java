673b912534b4fd464b5cdfca6a49f5b622e49881
fdez2002 <fdez2002@gmail.com>
2022-11-09 19:27:13 +0100

    Creacion de la pantalla de incio

--- app/src/main/java/com/example/ejerciciobbddnavigation/MainActivity.java
+++ app/src/main/java/com/example/ejerciciobbddnavigation/MainActivity.java
@@ -29,19 +29,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         setSupportActionBar(binding.appBarMain.toolbar);
-        binding.appBarMain.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
         DrawerLayout drawer = binding.drawerLayout;
         NavigationView navigationView = binding.navView;
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_inicio, R.id.nav_matricular, R.id.nav_modificar)
+                R.id.nav_inicio, R.id.nav_matricular, R.id.nav_modificar, R.id.nav_borrar)
                 .setOpenableLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
@@ -49,12 +43,6 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupWithNavController(navigationView, navController);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
 
     @Override
     public boolean onSupportNavigateUp() {
--- app/src/main/java/com/example/ejerciciobbddnavigation/ui/inicio/InicioFragment.java
+++ app/src/main/java/com/example/ejerciciobbddnavigation/ui/inicio/InicioFragment.java
@@ -24,8 +24,8 @@ public class InicioFragment extends Fragment {
         binding = FragmentInicioBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+       //final TextView textView = binding.textViewBienvenidos;
+        //homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
