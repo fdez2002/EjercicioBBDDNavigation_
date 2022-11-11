7a60df1781231d1440a35705019d2f6931b28b57
fdez2002 <fdez2002@gmail.com>
2022-11-11 19:23:18 +0100

    Cambio a la hora de limpiar los editText

--- app/src/main/java/com/example/ejerciciobbddnavigation/ui/modificar/ModificarFragment.java
+++ app/src/main/java/com/example/ejerciciobbddnavigation/ui/modificar/ModificarFragment.java
@@ -81,10 +81,10 @@ public class ModificarFragment extends Fragment {
                     //Luego se llama al método update de la clase SQLiteDatabase pasando el nombre de la tabla,
                     // el objeto de la clase ContentValues y la condición del where (el cuanto parámetro en este ejemplo no se lo emplea)
                     int cantidad = BaseDeDatos.update("alumnos", registro, "dni='"+dni+"'", null);
-                    //limpiar();
+
                     //Dependiendo del resultado de cantidad informamos al usuario
                     if (cantidad ==1){
-
+                        limpiar();
                         Toast.makeText(thisContext, "Registro modificado correctamente", Toast.LENGTH_SHORT).show();
                     } else{
                         Toast.makeText(thisContext, "No existe el alumno", Toast.LENGTH_SHORT).show();
